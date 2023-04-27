5f9e7e2039872e806123da5d160cc1da69ca818e
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-04-27 17:45:16 +0300

    Добавьте файлы проекта.

---
+++ Barsik oop.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal{ 
+	public:
+	int legs = 4;
+	string name = "";
+	string type;
+
+	void run() {
+		cout << name + " start running \n";
+    }
+
+};
+
+
+
+int main()
+{
+	Animal cat = {4 ,"Barsik","kot"};
+	cat.run();
+
+   system("pause>0");
+}
