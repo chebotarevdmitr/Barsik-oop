8e7523c00c5ab319e89fe4ff750c945f62997125
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-04-27 18:25:09 +0300

    setter

--- Barsik oop.cpp
+++ Barsik oop.cpp
@@ -1,16 +1,25 @@
 ﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
 class Animal{ 
-	public:
+	//  1  public:
 	int legs = 4;
 	string name = "";
 	string type;
 
-	void run() {
-		cout << name + " start running \n";
-    }
+	// 1 void run() {
+	// 1 cout << name + " start running \n";
+    //    }
+	// 
+    //set funkziya izmenyet pole harakteristiku givotnogo
+
+	void set_legs(int new_legs) {
+		if (new_legs >= 0 && new_legs <= 1000) {
+			legs = new_legs;
+		}
+	}
 
 };
 
@@ -18,8 +27,12 @@ class Animal{
 
 int main()
 {
-	Animal cat = {4 ,"Barsik","kot"};
-	cat.run();
+	// 1 Animal cat = {4 ,"Barsik","kot"};
+	// 1 cat.run();
 
+	Animal cat ;
+
+
+	
    system("pause>0");
 }
